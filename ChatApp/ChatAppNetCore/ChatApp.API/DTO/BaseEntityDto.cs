08fdd625fa5abec5059d1f310c49a702dc9ee1fc
Eduard Ioan <rotaru15@gmail.com>
2022-01-09 10:11:42 +0200

    cleaning

---
+++ ChatApp/ChatAppNetCore/ChatApp.API/DTO/BaseEntityDto.cs
@@ -0,0 +1,8 @@
+namespace ChatApp.API.DTO
+{
+    public class BaseEntityDto
+    {       
+        public int Id {get;set;}
+        public string Name{get;set;}
+    }
+}
---
+++ ChatApp/ChatAppNetCore/ChatApp.API/DTO/ImageDto.cs
@@ -0,0 +1,7 @@
+namespace ChatApp.API.DTO
+{
+    public class ImageDto : BaseEntityDto
+    {
+        public byte[] Data { get; set; }
+    }
+}
