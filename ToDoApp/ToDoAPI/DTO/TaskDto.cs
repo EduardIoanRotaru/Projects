11231e7491831b4a0b860bd24ebeecc855c03056
EduardIoanRotaru <rotaru15@gmail.com>
2022-01-10 13:05:14 +0200

    ToDoApp cleaned up

---
+++ ToDoApp/ToDoAPI/DTO/TaskDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoAPI.DTO
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public ICollection<CommentDto> Comments { get; set; }
+    }
+}
---
+++ ToDoApp/client/src/app/shared/models/ITask.ts
@@ -0,0 +1,4 @@
+export class ITask {
+    id: string;
+    name: string;
+}
